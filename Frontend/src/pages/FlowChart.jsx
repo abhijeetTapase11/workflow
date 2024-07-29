96553915b5326b48381d6293e3200690cc767300
Anuj2906 <anujgupta8955@gmail.com>
2024-07-29 17:58:06 +0530

    fixed mapping in flowchart

--- Frontend/src/pages/FlowChart.jsx
+++ Frontend/src/pages/FlowChart.jsx
@@ -1,16 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
   ReactFlow,
   Background,
-  useNodesState,
-  useEdgesState,
-  addEdge
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { Container, Card } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-
-
+import { cloneDeep } from 'lodash';
 
 // const data = {
 //   "id": {
@@ -53,38 +49,62 @@ import { useSelector } from 'react-redux';
 // };
 
 export default function FlowChart() {
-  const data = useSelector((state) => state.workflow.resultData);
-  console.log("resultData1", data);
+  let resultData = useSelector((state) => state.workflow.resultData);
+  const data = cloneDeep(resultData);
+  console.log("resultData1", typeof(data));
 
   const color1 = "#6ede87";
   const color2 = "#D6D5E6";
 
+  for (let i = 0; i < data.tasks.length; i++) {
+    let trueCondition = "";
+    let falseCondition = "";
+    console.log(trueCondition, falseCondition);
+    if (data.tasks[i].api_check === "DOB check") {
+      trueCondition = `age > ${data.tasks[i].condition}`;
+      falseCondition = `age < ${data.tasks[i].condition}`;
+    }
+    if (data.tasks[i].api_check === "Gender check") {
+      if (data.tasks[i].condition === "M") {
+        trueCondition = "Male";
+        falseCondition = "Female";
+      } else {
+        trueCondition = "Female";
+        falseCondition = "Male";
+      }
+    }
+    if (data.tasks[i].api_check === "Pincode check") {
+      trueCondition = `Pincode = ${data.tasks[i].condition}xxxx`;
+      falseCondition = `Pincode != ${data.tasks[i].condition}xxxx`;
+    }
+    if (trueCondition && falseCondition) {
+      data.tasks[i].condition = [trueCondition, falseCondition];
+      console.log(data.tasks[i].condition);
+    }
+  }
+
   const initialNodes = [
     { id: "1", type: "input", data: { label: data.tasks[0].api_check }, position: { x: 250, y: 0 }, style: { backgroundColor: color1 } },
-    { id: "2", data: { label: "loan approved" }, position: { x: 100, y: 100 }, style: { backgroundColor: data.tasks[0].status === "failure" ? color2 : color1 } },
-    { id: "3", data: { label: data.tasks[1].api_check }, position: { x: 400, y: 100 }, style: { backgroundColor: data.tasks[0].status !== "failure" ? color1 : color2 } },
-    { id: "4", data: { label: "loan approved" }, position: { x: 300, y: 200 }, style: { backgroundColor: !(data.tasks[1].status === "failure") && data.tasks[0].status === "failure" ? color1 : color2 } },
-    { id: "5", data: { label: data.tasks[2].api_check }, position: { x: 500, y: 200 }, style: { backgroundColor: data.tasks[1].status === "failure" && data.tasks[0].status === "failure" ? color1 : color2 } },
-    { id: "6", data: { label: "loan approved" }, position: { x: 400, y: 300 }, style: { backgroundColor: !(data.tasks[2].status === "failure") && data.tasks[1].status === "failure" && data.tasks[0].status === "failure" ? color1 : color2 } },
-    { id: "7", data: { label: "loan approval required" }, position: { x: 600, y: 300 }, style: { backgroundColor: data.tasks[2].status === "failure" && data.tasks[1].status === "failure" && data.tasks[0].status === "failure" ? color1 : color2 } }
+    { id: "2", data: { label: "loan approved" }, position: { x: 100, y: 100 }, style: { backgroundColor: data.tasks[0].status === "failure" ? color1 : color2 } },
+    { id: "3", data: { label: data.tasks[1].api_check }, position: { x: 400, y: 100 }, style: { backgroundColor: data.tasks[0].status === "success" ? color1 : color2 } },
+    { id: "4", data: { label: "loan approved" }, position: { x: 300, y: 200 }, style: { backgroundColor: data.tasks[1].status === "failure" && data.tasks[0].status === "success" ? color1 : color2 } },
+    { id: "5", data: { label: data.tasks[2].api_check }, position: { x: 500, y: 200 }, style: { backgroundColor: data.tasks[1].status === "success" && data.tasks[0].status === "success" ? color1 : color2 } },
+    { id: "6", data: { label: "loan approval required" }, position: { x: 400, y: 300 }, style: { backgroundColor: data.tasks[2].status === "failure" && data.tasks[1].status === "success" && data.tasks[0].status === "success" ? color1 : color2 } },
+    { id: "7", data: { label: "loan approved" }, position: { x: 600, y: 300 }, style: { backgroundColor: data.tasks[2].status === "success" && data.tasks[1].status === "success" && data.tasks[0].status === "success" ? color1 : color2 } }
   ];
 
   const initialEdges = [
-    { id: "e1-2", source: "1", target: "2", label: `age < ${data.tasks[0].condition}` },
-    { id: "e1-3", source: "1", target: "3", label: `age > ${data.tasks[0].condition}` },
-    { id: "e3-4", source: "3", target: "4", label: "Female" },
-    { id: "e3-5", source: "3", target: "5", label: "Male" },
-    { id: "e5-6", source: "5", target: "6", label: `pincode != ${data.tasks[2].condition}xxxx` },
-    { id: "e5-7", source: "5", target: "7", label: `pincode = ${data.tasks[2].condition}xxxx` }
+    { id: "e1-2", source: "1", target: "2", label: data.tasks[0].condition[1] },
+    { id: "e1-3", source: "1", target: "3", label: data.tasks[0].condition[0] },
+    { id: "e3-4", source: "3", target: "4", label: data.tasks[1].condition[1] },
+    { id: "e3-5", source: "3", target: "5", label: data.tasks[1].condition[0] },
+    { id: "e5-6", source: "5", target: "6", label: data.tasks[2].condition[0] },
+    { id: "e5-7", source: "5", target: "7", label: data.tasks[2].condition[1] }
   ];
 
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const nodes = initialNodes;
+  const edges = initialEdges;
 
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
-  );
   
   return (
     <Container fluid className="p-3">
@@ -95,9 +115,6 @@ export default function FlowChart() {
             <ReactFlow
               nodes={nodes}
               edges={edges}
-              onNodesChange={onNodesChange}
-              onEdgesChange={onEdgesChange}
-              onConnect={onConnect}
               fitView
             >
               <Background variant="dots" gap={12} size={1} />
